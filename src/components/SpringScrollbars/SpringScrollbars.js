6cc484246b4aaddec33ced486e15f14edf15fca8
Ben Ilegbodu <ben@benmvp.com>
2018-04-04 19:00:47 -0700

    Update ListItem to display gray fallback circle when cover is missing

--- src/components/SpringScrollbars/SpringScrollbars.js
+++ src/components/SpringScrollbars/SpringScrollbars.js
@@ -1,77 +1,81 @@
-import React, { Component } from "react";
-import { Scrollbars } from "react-custom-scrollbars";
-import PropTypes from "prop-types";
-import { SpringSystem, MathUtil } from "rebound";
-import { forceCheck } from "react-lazyload";
-import { connect } from "react-redux";
+import React, {Component} from 'react'
+import {Scrollbars} from 'react-custom-scrollbars'
+import PropTypes from 'prop-types'
+import {SpringSystem, MathUtil} from 'rebound'
+import {forceCheck} from 'react-lazyload'
+import {connect} from 'react-redux'
 
-import { setScrollToTop } from "../../state/store";
+import {setScrollToTop} from '../../state/store'
 
 class SpringScrollbars extends Component {
   constructor(props, ...rest) {
-    super(props, ...rest);
-    this.handleSpringUpdate = this.handleSpringUpdate.bind(this);
+    super(props, ...rest)
+    this.handleSpringUpdate = this.handleSpringUpdate.bind(this)
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (this.props.isNavigator && this.props.navigatorPosition !== "is-featured") {
-      return;
+    if (this.props.isNavigator && this.props.navigatorPosition !== 'is-featured') {
+      return
     }
 
     if (this.props.scrollToTop && this.props.scrollToTop !== prevProps.scrollToTop) {
-      this.scrollTop(0);
-      this.props.setScrollToTop(false);
+      this.scrollTop(0)
+      this.props.setScrollToTop(false)
     }
   }
   componentDidMount() {
-    this.springSystem = new SpringSystem();
-    this.spring = this.springSystem.createSpring();
-    this.spring.addListener({ onSpringUpdate: this.handleSpringUpdate });
+    this.springSystem = new SpringSystem()
+    this.spring = this.springSystem.createSpring()
+    this.spring.addListener({onSpringUpdate: this.handleSpringUpdate})
   }
 
   componentWillUnmount() {
-    this.springSystem.deregisterSpring(this.spring);
-    this.springSystem.removeAllListeners();
-    this.springSystem = undefined;
-    this.spring.destroy();
-    this.spring = undefined;
+    if (this.springSystem) {
+      this.springSystem.deregisterSpring(this.spring)
+      this.springSystem.removeAllListeners()
+      this.springSystem = undefined
+    }
+    if (this.spring) {
+      this.spring.destroy()
+      this.spring = undefined
+    }
   }
 
   getScrollTop() {
-    return this.scrollbars.getScrollTop();
+    return this.scrollbars.getScrollTop()
   }
 
   getScrollHeight() {
-    return this.scrollbars.getScrollHeight();
+    return this.scrollbars.getScrollHeight()
   }
 
   getHeight() {
-    return this.scrollbars.getHeight();
+    return this.scrollbars.getHeight()
   }
 
   scrollTop(top) {
-    const scrollTop = this.scrollbars.getScrollTop();
-    const scrollHeight = this.scrollbars.getScrollHeight();
+    const scrollTop = this.scrollbars.getScrollTop()
+    const scrollHeight = this.scrollbars.getScrollHeight()
     const val = MathUtil.mapValueInRange(
       top,
       0,
       scrollHeight,
       scrollHeight * 0.01,
-      scrollHeight * 0.99
-    );
-    this.spring.setCurrentValue(scrollTop).setAtRest();
-    this.spring.setEndValue(val);
+      scrollHeight * 0.99,
+    )
+    this.spring.setCurrentValue(scrollTop).setAtRest()
+    this.spring.setEndValue(val)
   }
 
   handleSpringUpdate(spring) {
     window.requestAnimationFrame(() => {
-      const val = spring.getCurrentValue();
-      this.scrollbars.scrollTop(val);
-    });
+      const val = spring.getCurrentValue()
+      this.scrollbars.scrollTop(val)
+    })
   }
 
   render() {
-    const { children, forceCheckOnScroll } = this.props;
+    const {children, forceCheckOnScroll} = this.props
 
     return (
       <Scrollbars
@@ -79,12 +83,12 @@ class SpringScrollbars extends Component {
         universal={true}
         onScroll={forceCheckOnScroll && forceCheck}
         ref={comp => {
-          this.scrollbars = comp;
+          this.scrollbars = comp
         }}
       >
         {children}
       </Scrollbars>
-    );
+    )
   }
 }
 
@@ -94,18 +98,18 @@ SpringScrollbars.propTypes = {
   setScrollToTop: PropTypes.func.isRequired,
   forceCheckOnScroll: PropTypes.bool,
   navigatorPosition: PropTypes.string.isRequired,
-  isNavigator: PropTypes.bool
-};
+  isNavigator: PropTypes.bool,
+}
 
 const mapStateToProps = (state, ownProps) => {
   return {
     scrollToTop: state.scrollToTop,
-    navigatorPosition: state.navigatorPosition
-  };
-};
+    navigatorPosition: state.navigatorPosition,
+  }
+}
 
 const mapDispatchToProps = {
-  setScrollToTop
-};
+  setScrollToTop,
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(SpringScrollbars);
+export default connect(mapStateToProps, mapDispatchToProps)(SpringScrollbars)
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,7 @@
-import { createMuiTheme } from "material-ui/styles";
-import Color from "color";
+import {createMuiTheme} from 'material-ui/styles'
+import Color from 'color'
 
-const colors = require("./colors");
+const colors = require('./colors')
 
 const theme = createMuiTheme({
   base: {
@@ -13,16 +13,16 @@ const theme = createMuiTheme({
         .lighten(0.1)
         .string(),
       accent: colors.accent,
-      lines: colors.superLightGray
+      lines: colors.superLightGray,
     },
     sizes: {
-      linesMargin: "20px"
+      linesMargin: '20px',
     },
     fonts: {
       unstyledFamily: `Arial`,
-      styledFamily: "Open Sans",
-      styledFonts: "300,400,600"
-    }
+      styledFamily: 'Open Sans',
+      styledFonts: '300,400,600',
+    },
   },
   info: {
     colors: {
@@ -31,32 +31,33 @@ const theme = createMuiTheme({
       socialIcons: colors.lightGray,
       socialIconsHover: colors.accent,
       menuLink: colors.gray,
-      menuLinkHover: colors.accent
+      menuLinkHover: colors.accent,
     },
     sizes: {
       width: 320,
-      headerHeight: 170
+      headerHeight: 170,
     },
     fonts: {
       boxTitleSize: 1.3,
       boxTitleSizeM: 1.5,
-      boxTitleSizeL: 1.7
-    }
+      boxTitleSizeL: 1.7,
+    },
   },
   navigator: {
     colors: {
       background: colors.white,
       postsListItemLink: colors.gray,
       postsListItemLinkHover: colors.accent,
-      postsHeader: colors.gray
+      postsListItemPointerBackground: colors.lightGray,
+      postsHeader: colors.gray,
     },
     sizes: {
       closedHeight: 80,
       postsListItemH1Font: 1.3,
       postsListItemH2Font: 1.1,
       fontIncraseForM: 1.15,
-      fontIncraseForL: 1.3
-    }
+      fontIncraseForL: 1.3,
+    },
   },
   main: {
     colors: {
@@ -69,10 +70,10 @@ const theme = createMuiTheme({
       contentHeading: colors.gray,
       blockquoteFrame: colors.lightGray,
       link: colors.accent,
-      linkHover: colors.dark
+      linkHover: colors.dark,
     },
     sizes: {
-      articleMaxWidth: "50em"
+      articleMaxWidth: '50em',
     },
     fonts: {
       title: {
@@ -80,36 +81,36 @@ const theme = createMuiTheme({
         sizeM: 2.5,
         sizeL: 2.7,
         weight: 600,
-        lineHeight: 1.1
+        lineHeight: 1.1,
       },
       subTitle: {
         size: 1.5,
         sizeM: 1.8,
         sizeL: 1.95,
         weight: 300,
-        lineHeight: 1.1
+        lineHeight: 1.1,
       },
       meta: {
         size: 0.9,
-        weight: 600
+        weight: 600,
       },
       content: {
         size: 1.0,
         sizeM: 1.15,
         sizeL: 1.1,
-        lineHeight: 1.6
+        lineHeight: 1.6,
       },
       contentHeading: {
         h2Size: 1.5,
         h3Size: 1.3,
         weight: 600,
-        lineHeight: 1.3
+        lineHeight: 1.3,
       },
       footer: {
         size: 1,
-        lineHeight: 1.4
-      }
-    }
+        lineHeight: 1.4,
+      },
+    },
   },
   footer: {
     colors: {
@@ -119,42 +120,42 @@ const theme = createMuiTheme({
       link: colors.accent,
       linkHover: Color(colors.accent)
         .lighten(0.2)
-        .string()
+        .string(),
     },
     fonts: {
       footnote: {
         size: 0.8,
-        lineHeight: 1.4
-      }
-    }
+        lineHeight: 1.4,
+      },
+    },
   },
   bars: {
     colors: {
-      background: colors.white
+      background: colors.white,
     },
     sizes: {
       actionsBar: 60,
-      infoBar: 60
-    }
+      infoBar: 60,
+    },
   },
   mediaQueryTresholds: {
     M: 600,
-    L: 1024
+    L: 1024,
   },
   palette: {
     primary: {
-      main: "#709425"
-    }
+      main: '#709425',
+    },
   },
   typography: {
     fontFamily: `Arial, sans-serif`,
-    fontSize: 16
+    fontSize: 16,
   },
   pallete: {
     action: {
-      hover: "rgba(0, 0, 0, 0.01)"
-    }
-  }
-});
+      hover: 'rgba(0, 0, 0, 0.01)',
+    },
+  },
+})
 
-export default theme;
+export default theme
