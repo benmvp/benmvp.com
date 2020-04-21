dfd1bd3ee196f562d7256eb08acaf6a354227678
Ben Ilegbodu <ben@benmvp.com>
2020-04-20 20:43:16 -0700

    Add 2 Cor 5:17 to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -56,7 +56,15 @@ const Footer = () => {
         </GatsbyLink>
       </Typography>
       <Typography variant="body2" align="center">
-        © 2015-{new Date().getFullYear()}, Ben Ilegbodu. All rights reserved.
+        © 2015-{new Date().getFullYear()}, Ben Ilegbodu. All rights reserved.{' '}
+        <Link
+          href="https://www.biblegateway.com/passage/?search=2%20cor%205%3A17&version=NLT;NTV"
+          target="_blank"
+          rel="noopener noreferrer"
+          color="inherit"
+        >
+          2 Cor 5:17
+        </Link>
       </Typography>
       <Typography
         variant="caption"
