880ad67e609a89152a0be834ce2fc8ca74502b90
Ben Ilegbodu <ben@benmvp.com>
2020-05-17 02:19:31 -0700

    remove console.log

--- src/pages/minishops.tsx
+++ src/pages/minishops.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) =>
 const SpeakingEngagements = ({ data }) => {
   const classes = useStyles()
   const { hero, minishops } = data
-  console.log({ minishops })
+
   return (
     <Layout>
       <Seo
