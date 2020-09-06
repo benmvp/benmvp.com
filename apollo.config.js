6fefaee093bac05141f84bc366c386ee2058f052
Ben Ilegbodu <ben@benmvp.com>
2020-09-06 02:57:11 -0700

    Update a whole lot of packages

--- apollo.config.js
+++ apollo.config.js
@@ -6,10 +6,10 @@ module.exports = {
       './src/**/*.tsx',
       './src/**/*.ts',
       './plugins/**/*.js',
-      './node_modules/gatsby-source-contentful/src/*.js',
+      './node_modules/gatsby-source-contentful/src/fragments.js',
       './node_modules/gatsby-source-datocms/fragments/*.js',
-      './node_modules/gatsby-transformer-sharp/src/*.js',
-      './node_modules/gatsby-image/src/*.js',
+      './node_modules/gatsby-source-sanity/fragments/*.js',
+      './node_modules/gatsby-transformer-sharp/src/fragments.js',
     ],
     service: {
       name: 'gatsbySchema',
