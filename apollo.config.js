9455d3be6b3ef6eabe67c6743f8725b14c008eae
Ben Ilegbodu <ben@benmvp.com>
2020-09-06 01:31:47 -0700

    Add eslint caching

--- apollo.config.js
+++ apollo.config.js
@@ -2,11 +2,19 @@ module.exports = {
   client: {
     addTypename: false,
     excludes: [],
-    includes: ["./src/**/*.tsx","./src/**/*.ts","./plugins/**/*.js","./node_modules/gatsby-source-contentful/src/*.js","./node_modules/gatsby-source-datocms/fragments/*.js","./node_modules/gatsby-transformer-sharp/src/*.js","./node_modules/gatsby-image/src/*.js"],
+    includes: [
+      './src/**/*.tsx',
+      './src/**/*.ts',
+      './plugins/**/*.js',
+      './node_modules/gatsby-source-contentful/src/*.js',
+      './node_modules/gatsby-source-datocms/fragments/*.js',
+      './node_modules/gatsby-transformer-sharp/src/*.js',
+      './node_modules/gatsby-image/src/*.js',
+    ],
     service: {
-      name: "gatsbySchema",
-      localSchemaFile: "./schema.json"
+      name: 'gatsbySchema',
+      localSchemaFile: './schema.json',
     },
-    tagName: "graphql"
-  }
+    tagName: 'graphql',
+  },
 }
