46885ec2fc6312e7bbab30dac4238e9cceebed29
Ben Ilegbodu <ben@benmvp.com>
2022-01-06 20:42:54 -0800

    Fix env vas in bugsnag-build-reporter

--- scripts/bugsnag-build-reporter.js
+++ scripts/bugsnag-build-reporter.js
@@ -7,18 +7,24 @@ require('dotenv').config({
 const reportBuild = require('bugsnag-build-reporter')
 
 const reportBugsnagBuild = async () => {
+  const appVersion = process.env.VERCEL_GIT_COMMIT_SHA || process.env.DEPLOY_ID
+  const repository = process.env.VERCEL_GIT_REPO_SLUG
+    ? `https://github.com/${process.env.VERCEL_GIT_REPO_OWNER}/${process.env.VERCEL_GIT_REPO_SLUG}`
+    : process.env.REPOSITORY_URL
+
   try {
     await reportBuild({
       apiKey: process.env.GATSBY_BUGSNAG_API_KEY,
-      appVersion: process.env.DEPLOY_ID,
-      releaseStage: process.env.CONTEXT || process.env.NODE_ENV,
+      appVersion,
+      releaseStage:
+        process.env.VERCEL_ENV || process.env.CONTEXT || process.env.NODE_ENV,
       sourceControl: {
         provider: 'github',
-        repository: process.env.REPOSITORY_URL,
-        revision: process.env.COMMIT_REF,
+        repository,
+        revision: process.env.VERCEL_GIT_COMMIT_SHA || process.env.COMMIT_REF,
       },
     })
-    console.log('successfully reported build', process.env.DEPLOY_ID)
+    console.log('successfully reported build', appVersion)
   } catch (err) {
     console.error(err)
   }
